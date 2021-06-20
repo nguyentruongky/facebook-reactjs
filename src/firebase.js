ea5423219787a50d435fde789f7ab1ca6003a2e3
Ky Nguyen <nguyentruongky33@gmail.com>
2021-06-20 11:05:26 +0700

    Add header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyCoqCR_oFEeTVrRgcIr21D0pdRldoCSB2A',
+    authDomain: 'facebook-clone-79c3b.firebaseapp.com',
+    projectId: 'facebook-clone-79c3b',
+    storageBucket: 'facebook-clone-79c3b.appspot.com',
+    messagingSenderId: '587936808074',
+    appId: '1:587936808074:web:fcc7f9754b1431815e4173',
+}
